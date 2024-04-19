21850fc45c043e20e1fd4ba207c61452d912a911
MichaEL51161 <you@akintoyeoluwasegun001@gmail.com>
2024-04-19 11:31:43 +0100

    me commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFuncOpen() {
+    document.getElementById('bugger').style.display=('flex');
+    document.getElementById('Open').style.display=('none');
+}
+
+
+function myFuncClose() {
+    document.getElementById('bugger').style.display=('none');
+    document.getElementById('Open').style.display=('flex');
+}
